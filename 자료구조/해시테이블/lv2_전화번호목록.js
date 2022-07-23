a68ab0dac21285192fe3f51a6f866ebc120df00a
sangkoon <bigyou00@gmail.com>
2022-07-23 15:06:47 +0900

    해시 전화번호목록

--- 자료구조/해시테이블/lv2_전화번호목록.js
+++ 자료구조/해시테이블/lv2_전화번호목록.js
@@ -1,1 +1,21 @@
+// js 버전은 없어서 채점은 못함
+function solution(phone_list = []) {
+  // 배열을순회하여 map에 넣는다.
 
+  for (let i = 0; i < phone_list.length; i++) {
+    for (let j = i + 1; j < phone_list.length; j++) {
+      if (
+        phone_list[i].includes(phone_list[j]) ||
+        phone_list[j].includes(phone_list[i])
+      ) {
+        // 접두어인지 파악해야함
+        if (phone_list[j][0] === phone_list[i][0]) return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(solution(["119", "97674223", "1195524421"])); // false
+console.log(solution(["123", "456", "789"])); // true
+console.log(solution(["12", "123", "1235", "567", "88"])); // false
