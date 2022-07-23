bc99777c2d57b01576825d4eb5b36fa80fc22b2e
sangkoon <bigyou00@gmail.com>
2022-07-23 16:50:16 +0900

    위장 +2

--- temp.js
+++ temp.js
@@ -1,6 +1,7 @@
-const map = new Map([]);
-map.set("상훈", { 나이: 25, 지역: ["인천"] });
-// console.log(map);
+const obj = {};
+obj["상훈"] = [1];
 
-console.log(...map);
-console.log([...map.entries()]);
+console.log(obj);
+obj["상훈"].push(2);
+
+console.log(obj);
--- 자료구조/해시테이블/lv2_위장.js
+++ 자료구조/해시테이블/lv2_위장.js
@@ -1,1 +1,40 @@
+function solution(clothes = []) {
+  let map = new Map();
 
+  clothes.forEach(([name, category]) => {
+    if (map.has(category)) {
+      map.get(category).push(name);
+      map.get(category)[0]++;
+    } else {
+      map.set(category, [1, name]);
+    }
+  });
+  let temp = [];
+  map.forEach(([count]) => {
+    temp.push(count);
+  });
+
+  return temp.reduce((cur, acc) => cur * (acc + 1), 1) - 1;
+}
+
+console.log(
+  solution([
+    ["yellow_hat", "headgear"],
+    ["blue_sunglasses", "eyewear"],
+    ["green_turban", "headgear"],
+  ])
+); //5
+console.log(
+  solution([
+    ["crow_mask", "face"],
+    ["blue_sunglasses", "face"],
+    ["smoky_makeup", "face"],
+  ])
+); // 3
+
+// function fac(n) {
+//   if (n <= 1) {
+//     return 1;
+//   }
+//   return fac(n - 1) * n;
+// }
