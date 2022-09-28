9fbe6b73c5d6c423a5eb15bc62791886e3f1a4ff
sanghoon <bigyou00@gmail.com>
2022-09-28 20:58:34 +0900

    백준 입력 받는게 상당히 불편하다 프로그래머스 문제들 다 풀고 넘어가자

--- ㄱ도움되는 함수/소수 구하기.js
+++ ㄱ도움되는 함수/소수 구하기.js
@@ -15,7 +15,7 @@ function is_prime(num) {
   return true;
 }
 
-// 에라토스테네스의 체 :  가장 효과적임
+// 👇에라토스테네스의 체 :  가장 효과적임
 // 1부터 N까지 모든 소수를 구하는 경우 가장 효율적이다.
 function get_primes(num) {
   const prime = [false, false, ...Array(num - 1).fill(true)];
--- 연습장.js
+++
@@ -1,7 +0,0 @@
-let answer = "";
-
-for (let i = 1; i <= 3 + 1; i++) {
-  answer += "*".repeat(i);
-  answer += "\n";
-}
-console.log(answer);
