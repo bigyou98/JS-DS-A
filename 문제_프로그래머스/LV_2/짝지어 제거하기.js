122b980464987c1aa8481eab6ffc72bb7c081a6c
sanghoon <bigyou00@gmail.com>
2022-09-29 15:59:38 +0900

    스택을 활용함

--- 문제_프로그래머스/LV_2/짝지어 제거하기.js
+++ 문제_프로그래머스/LV_2/짝지어 제거하기.js
@@ -1,28 +1,13 @@
-function solution(s) {
-  let sArr = [...s];
-  while (true) {
-    let count = 1;
-    for (let i = 1; i < sArr.length; i++) {
-      const cur = sArr[i - 1];
-      const next = sArr[i];
-      // 알파벳이 2개 붙어있는 짝을 찾는다.
-      if (cur === next) {
-        // 제거한다
-        // 앞뒤 문자열을 이어 붙인다
-        sArr.splice(i - 1, 2);
-        break;
-      } else {
-        count++;
-        if (count === sArr.length) {
-          return 0;
-        }
-      }
-    }
-    if (sArr.length === 0) {
-      return 1;
+function solution(s = "") {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.length || stack[stack.length - 1] !== s[i]) {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
     }
   }
-}
 
-console.log(solution("baabaa")); //1
-console.log(solution("cdcd")); //0
+  return stack.length ? 0 : 1;
+}
