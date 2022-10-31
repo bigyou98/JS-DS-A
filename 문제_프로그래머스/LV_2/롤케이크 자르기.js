7e8db369d6a0d2f670cd0adcf8c8a28cb9b17e57
sanghoon <bigyou00@gmail.com>
2022-10-31 15:40:46 +0900

    +3 객체로는 풀리지 않았다.  key의 개수를 알기 위해선 Object.keys에 넣은 후 length를 확인해야했다.  하지만 Map은 자체 프로퍼티로 size를 가지고 있기에 해당 로직을 아낄수 있었다.

--- 문제_프로그래머스/LV_2/롤케이크 자르기.js
+++ 문제_프로그래머스/LV_2/롤케이크 자르기.js
@@ -4,16 +4,46 @@
  * 토핑의 번호
  * @returns {number}
  */
+
 function solution(topping) {
   let answer = 0;
-  for (let i = 1; i < topping.length; i++) {
-    let younger = topping.slice(0, i);
-    let bigger = topping.slice(i);
-    if (younger === bigger) {
-      answer++;
+  let me = new Map();
+  let brother = new Map();
+
+  // 초기화
+  for (let i = 0; i < topping.length; i++) {
+    let data = topping[i];
+    if (brother.has(data)) {
+      brother.set(data, brother.get(data) + 1);
+    } else {
+      brother.set(data, 1);
     }
   }
+  for (let i = 0; i < topping.length; i++) {
+    let data = topping[i];
+    // me한테는 추가해주기
+    if (!me.has(data)) {
+      me.set(data, 1);
+    }
+
+    // brother한테는 제거해주기
+    if (brother.has(data)) {
+      if (brother.get(data) === 1) {
+        brother.delete(data);
+      } else {
+        brother.set(data, brother.get(data) - 1);
+      }
+    }
+
+    if (brother.size === me.size) {
+      answer++;
+    } else if (brother.size < me.size) {
+      break;
+    }
+  }
+
   return answer;
 }
+
 console.log(solution([1, 2, 1, 3, 1, 4, 1, 2])); //2
 // console.log(solution([1, 2, 3, 1, 4])); //0
