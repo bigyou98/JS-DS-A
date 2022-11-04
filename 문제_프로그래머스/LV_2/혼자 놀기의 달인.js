2699d27babad2551de216a36904c4207ec2bb9b7
sanghoon <bigyou00@gmail.com>
2022-11-04 16:24:47 +0900

    +3
    2번 그룹이 없는 경우를 생각하지 못했음...!
    문제 제대로 잘 읽고 적어놓자

--- 문제_프로그래머스/LV_2/혼자 놀기의 달인.js
+++ 문제_프로그래머스/LV_2/혼자 놀기의 달인.js
@@ -30,14 +30,18 @@ function solution(cards) {
       index++;
     }
   }
-  console.log(temp);
+
   const answerArr = temp
     .split(",")
     .filter((item) => item !== "")
     .map((item) => item.length)
     .sort((a, b) => b - a);
-
+  if (answerArr.length === 1) {
+    return 0;
+  }
   return answerArr[0] * answerArr[1];
 }
 
-console.log(solution([8, 6, 3, 7, 2, 5, 1, 4]));
+// console.log(solution([8, 6, 3, 7, 2, 5, 1, 4]));
+console.log(solution([10, 5, 6, 7, 8, 9, 1, 2, 3, 4]));
+console.log(solution([2, 3, 1])); // 0
