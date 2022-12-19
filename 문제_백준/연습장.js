cbce1195c7f1a4ea5d9bac8015a4af1fa6f89ac0
sanghoon <bigyou00@gmail.com>
2022-12-19 20:52:40 +0900

    또 특이한점 문자열에 \n 포함해서 해당 문자열 정답으로 내기기

--- 문제_백준/연습장.js
+++ 문제_백준/연습장.js
@@ -1,19 +1,8 @@
-// let input = require("fs").readFileSync("/dev/stdin").toString();
-let [n, ...input] = require("fs")
-  .readFileSync("예제.txt")
-  .toString()
-  .split("\n");
-
-input = input.map((data) => {
-  return data.split(" ");
-});
-
+const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+let input = require("fs").readFileSync(filePath).toString();
+input = parseInt(input);
 let answer = "";
-for (let i = 0; i < input.length; i++) {
-  const [num, str] = input[i];
-  for (const s of str) {
-    answer += s.repeat(num);
-  }
-  console.log(answer);
-  answer = "";
+
+for (let i = 1; i <= input; i++) {
+  answer += i + "\n";
 }
